bc3d5820e8b91156c2d1b1c30c76aee78c7df1a2
zmichaels <zmichaels11@gmail.com>
2015-06-05 16:04:12 -0700

    initial commit

---
+++ src/com/longlinkislong/gloop/GLImage2D.java
@@ -0,0 +1,459 @@
+/*
+ * Copyright (c) 2015 zmichaels.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    zmichaels - initial API and implementation and/or initial documentation
+ */
+package com.longlinkislong.gloop;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+import java.nio.ShortBuffer;
+import java.nio.channels.FileChannel.MapMode;
+import java.util.Optional;
+
+/**
+ * The GLImage2D class provides a simple 2D image container intended for use as
+ * intermediate 2D texture data.
+ *
+ * @author zmichaels
+ * @since 15.02.02
+ */
+public class GLImage2D implements Closeable {
+
+    private static int[] TEMP = null;
+
+    protected final ByteBuffer data;
+    private final int[] size;
+    private Optional<Closeable> resource = Optional.empty();
+
+    /**
+     * Constructs a new GLImage with the specified width and height.
+     *
+     * @param width the width of the image. Must be greater than 1.
+     * @param height the height of the image. Must be greater than 1.
+     * @since 15.02.02
+     */
+    public GLImage2D(final int width, final int height) {
+        this.size = new int[]{width, height};
+        this.data = ByteBuffer.allocateDirect(width * height * 4).order(ByteOrder.nativeOrder());
+    }
+
+    /**
+     * Wraps the supplied data buffer as a GLImage2D
+     *
+     * @param width the width of the image
+     * @param height the height of the image
+     * @param data the data buffer
+     * @since 15.04.03
+     */
+    public GLImage2D(final int width, final int height, final ByteBuffer data) {
+        this.size = new int[]{width, height};
+        this.data = data;
+    }
+
+    /**
+     * Opens a RandomAccessFile as a GLImage2D. The entire file is then memory
+     * mapped.
+     *
+     * @param width the width of the image
+     * @param height the height of the image
+     * @param data the image file
+     * @param mode the mode to open the image
+     * @throws IOException if the file could not be opened
+     * @since 15.04.03
+     */
+    public GLImage2D(
+            final int width, final int height,
+            final RandomAccessFile data, final MapMode mode) throws IOException {
+
+        this.size = new int[]{width, height};
+        this.data = data.getChannel().map(mode, 0, width * height * 4);
+        this.resource = Optional.of(data);
+    }
+
+    /**
+     * Retrieves the total size of the image. This is the number of bytes that
+     * the image occupies.
+     *
+     * @return size of image in bytes.
+     * @since 15.02.02
+     */
+    public final int getSize() {
+        return this.size[0] * this.size[1] * 4;
+    }
+
+    /**
+     * Retrieves the width of the image measured in pixels.
+     *
+     * @return the width of the image in pixels.
+     * @since 15.02.02
+     */
+    public final int getWidth() {
+        return this.size[0];
+    }
+
+    /**
+     * Retrieves the height of the image measured in pixels.
+     *
+     * @return the height of the image in pixels
+     * @since 15.02.02
+     */
+    public final int getHeight() {
+        return this.size[1];
+    }
+
+    /**
+     * Calculates the offset index for the specified position
+     *
+     * @param x the x coordinate of the pixel
+     * @param y the y coordinate of the pixel
+     * @return The corresponding index location of the pixel.
+     * @since 15.02.02
+     */
+    protected final int indexOf(final int x, final int y) {
+        return x + y * this.getWidth();
+    }
+
+    /**
+     * Copies a rectangle of pixel information from the image to an int array.
+     *
+     * @param x the x coordinate of the upper left pixel location
+     * @param y the y coordinate of the upper left pixel location
+     * @param w the width of the pixel rectangle
+     * @param h the height of the pixel rectangle
+     * @param data the array to store the data in. If null is supplied an int
+     * array large enough to hold the rectangle will be created.
+     * @return The reference to an int array containing the rectangle of pixel
+     * information.
+     * @since 15.02.02
+     */
+    public final int[] getData(
+            final int x, final int y,
+            final int w, final int h,
+            final int[] data) {
+
+        final int[] out = data == null ? new int[w * h] : data;
+        final IntBuffer src = this.data.asIntBuffer();
+        final int scanlineStride = this.getWidth();
+        int yOff = indexOf(x, y);
+        int off = 0;
+
+        for (int yStart = 0; yStart < h; yStart++) {
+            src.position(yOff);
+            src.get(out, off, w);
+            off += w;
+            yOff += scanlineStride;
+        }
+
+        return out;
+    }
+
+    /**
+     * Copies a rectangle of pixel information from the image to a float array.
+     *
+     * @param x the x coordinate of the upper left pixel location
+     * @param y the y coordinate of the upper left pixel location
+     * @param w the width of the pixel rectangle
+     * @param h the height of the pixel rectangle
+     * @param data the array to store the data in. If null is supplied a float
+     * array large enough to hold the rectangle will be created.
+     * @return The reference to a float array containing the rectangle of pixel
+     * information.
+     * @since 15.02.02
+     */
+    public final float[] getData(
+            final int x, final int y,
+            final int w, final int h,
+            final float[] data) {
+
+        final float[] out = data == null ? new float[w * h] : data;
+        final FloatBuffer src = this.data.asFloatBuffer();
+        final int scanlineStride = this.getWidth();
+        int yOff = indexOf(x, y);
+        int off = 0;
+
+        for (int yStart = 0; yStart < h; yStart++) {
+            src.position(yOff);
+            src.get(out, off, w);
+            off += w;
+            yOff += scanlineStride;
+        }
+
+        return out;
+    }
+
+    /**
+     * Writes an int array of data to a rectangle of pixels.
+     *
+     * @param x the x coordinate of the upper left pixel location.
+     * @param y the y coordinate of the upper left pixel location.
+     * @param w the width of the pixel rectangle.
+     * @param h the height of the pixel rectangle.
+     * @param data the array to read data from.
+     * @since 15.02.02
+     */
+    public final void setData(
+            final int x, final int y,
+            final int w, final int h,
+            final int[] data) {
+
+        final IntBuffer dst = this.data.asIntBuffer();
+        final int scanlineSize = this.getWidth();
+        int yOff = indexOf(x, y);
+        int off = 0;
+
+        for (int yStart = 0; yStart < h; yStart++) {
+            dst.position(yOff);
+            dst.put(data, off, w);
+            off += w;
+            yOff += scanlineSize;
+        }
+    }
+
+    /**
+     * Writes a segment of an int array of data to a rectangle of pixels.
+     *
+     * @param dstX the x coordinate of the upper left pixel to write
+     * @param dstY the y coordinate of the upper left pixel to write
+     * @param dstW the width of the pixel rectangle
+     * @param dstH the height of the pixel rectangle
+     * @param srcX the x coordinate of the upper left pixel to read
+     * @param srcY the y coordinate of the upper left pixel to read
+     * @param data the array to read data from.
+     * @since 15.02.05
+     */
+    public final void setData(
+            final int dstX, final int dstY,
+            final int dstW, final int dstH,
+            final int srcX, final int srcY,
+            final int[] data) {
+
+        final IntBuffer dst = this.data.asIntBuffer();
+        final int scanlineSize = this.getWidth();
+        int dstYOff = indexOf(dstX, dstY);
+        int dstXOff = srcX + srcY * dstW;
+
+        for (int yStart = 0; yStart < dstH; yStart++) {
+            dst.position(dstYOff);
+            dst.put(data, dstXOff, dstW);
+            dstXOff += dstW;
+            dstYOff += scanlineSize;
+        }
+    }
+
+    /**
+     * Writes a float array of data to a rectangle of pixels.
+     *
+     * @param x the x coordinate of the upper left pixel location.
+     * @param y the y coordinate of the upper left pixel location.
+     * @param w the width of the pixel rectangle.
+     * @param h the height of the pixel rectangle.
+     * @param data the array to read data from.
+     * @since 15.02.02
+     */
+    public final void setData(
+            final int x, final int y,
+            final int w, final int h,
+            final float[] data) {
+
+        final FloatBuffer dst = this.data.asFloatBuffer();
+        final int scanlineSize = this.getWidth();
+        int yOff = indexOf(x, y);
+        int off = 0;
+
+        for (int yStart = 0; yStart < h; yStart++) {
+            dst.position(yOff);
+            dst.put(data, off, w);
+            off += w;
+            yOff += scanlineSize;
+        }
+    }
+
+    /**
+     * Writes a section of a float array of data to another rectangle.
+     *
+     * @param dstX the x coordinate of the upper left pixel to write.
+     * @param dstY the y coordinate of the upper left pixel to write.
+     * @param dstW the width of the pixel rectangle.
+     * @param dstH the height of the pixel rectangle.
+     * @param srcX the x coordinate of the upper left pixel to read.
+     * @param srcY the y coordinate of the upper left pixel to read.
+     * @param data the array to read data from.
+     * @since 15.02.05
+     */
+    public final void setData(
+            final int dstX, final int dstY,
+            final int dstW, final int dstH,
+            final int srcX, final int srcY,
+            final float[] data) {
+
+        final FloatBuffer dst = this.data.asFloatBuffer();
+        final int scanlineSize = this.getWidth();
+        int yOff = indexOf(dstX, dstY);
+        int xOff = srcX + srcY * dstW;
+
+        for (int yStart = 0; yStart < dstH; yStart++) {
+            dst.position(yOff);
+            dst.put(data, xOff, dstW);
+            xOff += dstW;
+            yOff += scanlineSize;
+        }
+    }
+
+    /**
+     * Writes a GLImage to another GLImage.
+     *
+     * @param x the x coordinate of the upper left pixel location.
+     * @param y the y coordinate of the upper left pixel location.
+     * @param w the width of the pixel rectangle.
+     * @param h the height of the pixel rectangle.
+     * @param data the array to read data from
+     * @since 15.02.02
+     */
+    public final void setData(
+            final int x, final int y,
+            final int w, final int h,
+            final GLImage2D data) {
+
+        setData(x, y, w, h, 0, 0, data);
+    }
+
+    /**
+     * Writes a section of a GLImage to another GLImage.
+     *
+     * @param dstX the x coordinate of the upper left pixel to write
+     * @param dstY the y coordinate of the upper left pixel to write
+     * @param dstW the width of the pixel rectangle
+     * @param dstH the height of the pixel rectangle
+     * @param srcX the x coordinate of the upper left pixel to read
+     * @param srcY the y coordinate of the upper left pixel to read
+     * @param data the array to read data from.
+     * @since 15.02.05
+     */
+    public final void setData(
+            final int dstX, final int dstY,
+            final int dstW, final int dstH,
+            final int srcX, final int srcY,
+            final GLImage2D data) {
+
+        final IntBuffer src = data.data.asIntBuffer();
+        final IntBuffer dst = this.data.asIntBuffer();
+        final int[] tmp;
+        final int scanlineSize = dstW;
+
+        if (TEMP == null || TEMP.length < scanlineSize) {
+            TEMP = new int[scanlineSize];
+        }
+
+        tmp = TEMP;
+
+        for (int yStart = 0; yStart < dstH; yStart++) {
+            int destOff = indexOf(dstX, dstY + yStart);
+            int srcOff = data.indexOf(srcX, srcY + yStart);
+
+            dst.position(destOff);
+            src.position(srcOff);
+            src.get(tmp, 0, scanlineSize);
+            dst.put(tmp, 0, scanlineSize);
+        }
+    }
+
+    /**
+     * Retrieves the data of this GLImage as a ByteBuffer object.
+     *
+     * @return A ByteBuffer holding the same data as this object.
+     * @since 15.02.02
+     */
+    public final ByteBuffer asByteBuffer() {
+        return this.data.asReadOnlyBuffer();
+    }
+
+    /**
+     * Retrieves the data of this GLImage as a read-only ShortBuffer object.
+     *
+     * @return A read-only ShortBuffer holding the same data as this object.
+     * @since 15.02.02
+     */
+    public final ShortBuffer asShortBuffer() {
+        return this.data.asShortBuffer().asReadOnlyBuffer();
+    }
+
+    /**
+     * Retrieves the data of this GLImage as a read-only IntBuffer object.
+     *
+     * @return A read-only IntBuffer holding the same data as this object.
+     */
+    public final IntBuffer asIntBuffer() {
+        this.data.flip();
+        return this.data.asIntBuffer().asReadOnlyBuffer();
+    }
+
+    /**
+     * Retrieves the data of this GLImage as a read-only FloatBuffer object.
+     *
+     * @return A read-only IntBuffer holding the same data as this object.
+     * @since 15.02.02
+     */
+    public final FloatBuffer asFloatBuffer() {
+        this.data.flip();
+        return this.data.asFloatBuffer().asReadOnlyBuffer();
+    }    
+
+    /**
+     * Updates the specified 2D texture with this GLImage. This will copy a
+     * rectangle of pixels specified by the offset and size of this image to the
+     * specified texture. This calls GLTexture.update passing it the data and
+     * size of the GLImage.
+     *
+     * @param texture the texture to update.
+     * @param dataType the data type to treat the image as.
+     * @param offset the xy coordinate of the upper left pixel of the update
+     * rectangle.
+     * @since 15.02.02
+     */
+    public final void updateTexture(
+            final GLTexture texture,   
+            final GLType dataType,
+            final int[] offset) {
+
+        this.data.flip();
+        
+        texture.updateImage(0, offset[0], offset[1], GLTextureFormat.GL_RGBA, dataType, data);          
+    }
+
+    /**
+     * Writes the GLImage to a 2D texture. This will call GLTexture.loadTexture
+     * passing it the data and size of the GLImage.
+     *
+     * @param texture the texture to write the image data to.
+     * @param dataType the data type to treat the GLImage data as.
+     * @since 15.02.02
+     */
+    public final void writeToTexture(
+            final GLTexture texture,
+            final GLType dataType) {
+
+        this.data.flip();
+
+        texture.setImage(0, GLTextureInternalFormat.GL_RGBA, GLTextureFormat.GL_RGBA, this.size[0], this.size[1], dataType, data);        
+    }
+
+    @Override
+    public void close() throws IOException {
+        if(this.resource.isPresent()) {
+            this.resource.get().close();
+            this.resource = Optional.empty();
+        }
+    }
+
+}
---
+++ src/com/longlinkislong/gloop/PingPong.java
@@ -0,0 +1,169 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.longlinkislong.gloop;
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+/**
+ * A data structure that manages two instances of a type for safe read/write.
+ * For proper use, it is recommended to do all write functions to the back
+ * object and then prior to read, swap the objects and read from the front. 
+ *
+ * @author zmichaels
+ * @param <T> the type
+ */
+public class PingPong<T> {
+
+    private T front;
+    private T back;
+
+    /**
+     * Applies the function to the front object owned by two PingPongs.
+     *
+     * @param <U> the type of the first PingPong object.
+     * @param <V> the type of the second PingPong object.
+     * @param action the action to perform on both
+     * @param a the first PingPong object
+     * @param b the second PingPong object
+     * @since 15.05.26
+     */
+    public static <U, V> void applyFront(
+            final BiConsumer<? super U, ? super V> action,
+            final PingPong<U> a, final PingPong<V> b) {
+
+        action.accept(a.front, b.front);
+    }
+
+    /**
+     * Applies the function to the back object owned by two PingPongs.
+     *
+     * @param <U> the type of the first PingPong object
+     * @param <V> the type of the second PingPong object
+     * @param action the action to perform
+     * @param a the first PingPong object
+     * @param b the second PingPong object
+     * @since 15.05.26
+     */
+    public static <U, V> void applyBack(
+            final BiConsumer<? super U, ? super V> action,
+            final PingPong<U> a, final PingPong<V> b) {
+
+        action.accept(a.front, b.back);
+    }
+
+    /**
+     * Applies a function to both the front and back of two PingPong objects
+     *
+     * @param <U> the type of the first PingPong object
+     * @param <V> the type of the second PingPong object
+     * @param action the action to perform
+     * @param a the first PingPong object
+     * @param b the second PingPong object
+     * @since 15.05.26
+     */
+    public static <U, V> void applyBoth(
+            final BiConsumer<? super U, ? super V> action,
+            final PingPong<U> a, final PingPong<V> b) {
+
+        action.accept(a.front, b.front);
+        action.accept(a.back, b.back);
+    }
+
+    /**
+     * Applies a function to two PingPong objects with interleaved inputs.
+     *
+     * @param <U> the type of the first PingPong object
+     * @param <V> the type of the second PingPong object
+     * @param action the action to perform
+     * @param a the first PingPong object
+     * @param b the second PingPong object
+     * @since 15.05.26
+     */
+    public static <U, V> void applyCrossover(
+            final BiConsumer<? super U, ? super V> action,
+            final PingPong<U> a, final PingPong<V> b) {
+
+        action.accept(a.front, b.back);
+        action.accept(a.back, b.front);
+    }
+
+    /**
+     * Constructs a new PingPong object backed by the two supplied objects.
+     * Usually these will be containers.
+     *
+     * @param front the front object
+     * @param back the back object
+     * @since 15.05.26
+     */
+    public PingPong(final T front, final T back) {
+        this.front = front;
+        this.back = back;
+    }
+
+    /**
+     * Retrieves the front object
+     *
+     * @return the front object
+     * @since 15.05.26
+     */
+    public T front() {
+        return this.front;
+    }
+
+    /**
+     * Retrieves the back object
+     *
+     * @return the back object
+     * @since 15.05.26
+     */
+    public T back() {
+        return this.back;
+    }
+
+    /**
+     * Applies a function to both the front and the back objects.
+     *
+     * @param action the action to perform
+     * @since 15.05.26
+     */
+    public void applyBoth(final Consumer<? super T> action) {
+        action.accept(this.front);
+        action.accept(this.back);
+    }
+
+    /**
+     * Applies a function to the front object
+     *
+     * @param action the action to perform
+     * @since 15.05.26
+     */
+    public void applyFront(final Consumer<? super T> action) {
+        action.accept(this.front);
+    }
+
+    /**
+     * Applies a function to the back object
+     *
+     * @param action the action to perform
+     * @since 15.05.26
+     */
+    public void applyBack(final Consumer<? super T> action) {
+        action.accept(this.back);
+    }
+
+    /**
+     * Swaps the front and the back objects
+     *
+     * @since 15.05.26
+     */
+    public void swap() {
+        final T temp = this.front;
+
+        this.front = this.back;
+        this.back = temp;
+    }
+}
